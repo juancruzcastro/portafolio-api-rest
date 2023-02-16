e2e35918d42fa0d946529fd72c0257c88c9def06
Juan Cruz Castro <juan.cruz.castro@kyndryl.com>
2023-02-15 23:34:49 -0300

    New: Método para guardar nuevos documentos.

--- controllers/project.js
+++ controllers/project.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Project = require('../models/project');
+
 var controller = {
     home: function(req, res){
         return res.status(200).send({
@@ -9,6 +11,25 @@ var controller = {
     test: function(req, res){
         return res.status(200).send({
             message: 'Soy el test del controlador de project'
+        });
+    },
+    saveProject: function(req, res){
+        var project = new Project();
+
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.year;
+        project.langs = params.langs;
+        project.image = null;
+
+        project.save((err, projectStored) => {
+            if(err) return res.status(500).send({message:'Error al guardar.'});
+
+            if(!projectStored) return res.status(404).send({message: 'No se pudo guardar el projecto.'});
+
+            return res.status(200).send({project: projectStored});
         });
     }
 };
